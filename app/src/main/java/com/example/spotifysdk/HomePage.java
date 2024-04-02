71b0a3e86665f559bd24e1186315d2326e68b3fa
jlee600 <jinseo775@gmail.com>
2024-04-02 13:19:23 -0400

    Created HomePage.java

---
+++ app/src/main/java/com/example/spotifysdk/HomePage.java
@@ -0,0 +1,6 @@
+package com.example.spotifysdk;
+
+public class HomePage {
+    //OnClick profile_btn: new tab w temporary homepage for our App
+    //displays user profile info (profile textview)
+}
