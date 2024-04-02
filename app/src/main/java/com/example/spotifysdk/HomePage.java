a2c1a84f5f181ed028034b7fc277332b66c2bc8d
jlee600 <jinseo775@gmail.com>
2024-04-02 13:28:41 -0400

    Crated Homepage java and xml

--- app/src/main/java/com/example/spotifysdk/HomePage.java
+++ app/src/main/java/com/example/spotifysdk/HomePage.java
@@ -1,6 +1,14 @@
 package com.example.spotifysdk;
 
-public class HomePage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity {
     //OnClick profile_btn: new tab w temporary homepage for our App
     //displays user profile info (profile textview)
+    protected void OnCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.homepage);
+    }
 }
