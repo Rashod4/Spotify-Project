4c349a802a4e4449681407dcb03dd6c0954e59aa
Rashod Abdurasulov <rashodbekabdurasulov@gmail.com>
2024-04-09 23:20:18 -0400

    added every spotify wrapped field into db and added images for the artists

--- app/src/main/java/com/example/spotifysdk/SpotifyWrapped.java
+++ app/src/main/java/com/example/spotifysdk/SpotifyWrapped.java
@@ -6,11 +6,38 @@ public class SpotifyWrapped {
     private List<String> topArtists;
     private List<String> topTracks;
     private List<String> topGenres;
+    private List<String> previewUrls;
+    private List<String> imageUrls;
+    private List<String> artistImageUrls;
+    private List<String> topAlbums;
 
-    public SpotifyWrapped(List<String> topArtists, List<String> topTracks, List<String> topGenres) {
+
+    public SpotifyWrapped(List<String> topArtists, List<String> topTracks, List<String> topGenres,
+                          List<String> previewUrls, List<String> imageUrls, List<String> artistImageUrls,
+                          List<String> topAlbums) {
         this.topArtists = topArtists;
         this.topTracks = topTracks;
         this.topGenres = topGenres;
+        this.previewUrls = previewUrls;
+        this.imageUrls = imageUrls;
+        this.artistImageUrls = artistImageUrls;
+        this.topAlbums = topAlbums;
+    }
+
+    public List<String> getPreviewUrls() {
+        return previewUrls;
+    }
+
+    public List<String> getImageUrls() {
+        return imageUrls;
+    }
+
+    public List<String> getArtistImageUrls() {
+        return artistImageUrls;
+    }
+
+    public List<String> getTopAlbums() {
+        return topAlbums;
     }
 
     public List<String> getTopArtists() {
@@ -24,4 +51,5 @@ public class SpotifyWrapped {
     public List<String> getTopGenres() {
         return topGenres;
     }
+
 }
